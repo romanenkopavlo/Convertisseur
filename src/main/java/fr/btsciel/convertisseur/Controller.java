6cd494f0bd0d7100e3dcb74cd30f5dc9d6c9996f
pavlo <paihitetty444@gmail.com>
2024-01-24 13:40:48 +0100

    Version

--- src/main/java/fr/btsciel/convertisseur/Controller.java
+++ src/main/java/fr/btsciel/convertisseur/Controller.java
@@ -1,12 +1,13 @@
 package fr.btsciel.convertisseur;
 
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -15,9 +16,30 @@ public class Controller implements Initializable {
     public TextField textField_Init;
     public TextField textField_Final;
     public Button buttonConvertion;
+    public double taux_Euro_Dollar;
+    public double taux_Euro_Livre;
+    public double taux_Euro_Yen;
+    public double valeur_Conversion;
+    public DecimalFormat df;
+    public ConversionDevise [] tabConversionDevise;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+    }
+    public void convertion(ActionEvent actionEvent) {
+
+    }
+    public void comboSelection(ActionEvent actionEvent) {
+
+    }
+    public void fabriqueDonnees() {
+
+    }
+    public void initConvertion(ConversionDevise conversionDevise) {
+
+    }
+    public void alerteFormat() {
 
     }
 }
--- src/main/java/fr/btsciel/convertisseur/ConversionDevise.java
+++ src/main/java/fr/btsciel/convertisseur/ConversionDevise.java
@@ -1,5 +1,50 @@
 package fr.btsciel.convertisseur;
 
 public class ConversionDevise {
+    private String prompt;
+    private String source;
+    private String cible;
+    private double taux;
 
+    public ConversionDevise(String prompt, String source, String cible, double taux) {
+        this.prompt = prompt;
+        this.source = source;
+        this.cible = cible;
+        this.taux = taux;
+    }
+
+    public ConversionDevise() {
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getCible() {
+        return cible;
+    }
+
+    public void setCible(String cible) {
+        this.cible = cible;
+    }
+
+    public double getTaux() {
+        return taux;
+    }
+
+    public void setTaux(double taux) {
+        this.taux = taux;
+    }
 }
